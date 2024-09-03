2c4394e24885a35314c41aea227346d8bda02ec9
Vishal Gupta <65777217+vishalgupta2k@users.noreply.github.com>
2024-09-03 12:49:40 +0530

    sign up api

--- controllers/users.js
+++ controllers/users.js
@@ -6,20 +6,18 @@ const signUp = async (req, res) => {
     return res.send({ sucess: false, message: "All fields are required" });
   }
 
-  // const oldUser = await User.findOne({ email });
-  // if (oldUser) {
-  //   return res.send({ sucess: false, message: "User already exist" });
-  // }
-  // const user = new User({
-  //   name,
-  //   email,
-  //   phoneNumber,
-  //   password,
-  // });
-  // await user.save();
-
-  res.send({ sucess: true, message: "user created" });
-  console.log(req.body);
+  const oldUser = await User.findOne({ email });
+  if (oldUser) {
+    return res.send({ sucess: false, message: "User already exist" });
+  }
+  const user = new User({
+    name,
+    email,
+    phoneNumber,
+    password,
+  });
+  await user.save();
+  res.send({ sucess: true, message: "user created successfully" });
 };
 
 module.exports = { signUp };
